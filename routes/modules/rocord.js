b062a195a5970bed2520edbb253e00a84a763eeb
june850713@gmail.com <june850713@gmail.com>
2022-06-21 20:38:32 +0800

    feat: modify category select in new page

--- routes/modules/rocord.js
+++ routes/modules/rocord.js
@@ -1,9 +1,17 @@
 const express = require('express')
 const router = express.Router()
+const CATEGORY = require('../../category.json').data
 
 // add new record
 router.get('/new', (req, res) => {
-  res.render('new')
+  const categorys = []
+  CATEGORY.forEach(category => {
+    categorys.push({
+      id: category.id,
+      name: category.name
+    })
+  })
+  res.render('new', { categorys })
 })
 
 router.post('/new', (req, res) => {
