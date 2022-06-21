f542a81a9f040a2256a4cf69acc7f384039437c9
june850713@gmail.com <june850713@gmail.com>
2022-06-21 23:14:54 +0800

    feat: add edit page

--- routes/modules/rocord.js
+++ routes/modules/rocord.js
@@ -15,4 +15,18 @@ router.post('/new', (req, res) => {
     .catch(err => console.log(err))
 })
 
+// edit record
+router.get('/edit/:id', async (req, res) => {
+  let categorys = await Category.find().lean().sort({ id: 'asc' })
+  const _id = req.params.id
+  Record.findOne({ _id })
+    .lean()
+    .then(record => {
+      const currentCategory = categorys.find(category => record.categoryId === category.id).name
+      categorys = categorys.filter(category => record.categoryId !== category.id)
+      res.render('edit', { record, categorys, currentCategory })
+    })
+    .catch(err => console.log(err))
+})
+
 module.exports = router
