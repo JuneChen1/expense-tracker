751a322009733636ca74e12098b560d31c13749b
june850713@gmail.com <june850713@gmail.com>
2022-06-22 01:02:46 +0800

    feat: add sort function

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -22,4 +22,23 @@ router.get('/', async (req, res) => {
     .catch(err => console.log(err))
 })
 
+router.get('/sort', async (req, res) => {
+  const categoryId = req.query.sort
+  const categories = await Category.find()
+  const sortName = categories.find(category => category.id.toString() === categoryId.toString()).name
+  let totalAmount = 0
+  Record.find({ categoryId })
+    .lean()
+    .then(records => {
+      records.forEach(record => {
+        const icon = categories.find(category => record.categoryId === category.id).icon
+        record.icon = icon
+        record.date = moment(record.date).format('YYYY/MM/DD')
+        totalAmount += Number(record.amount)
+      })
+      res.render('index', { records, totalAmount, sortName })
+    })
+  .catch(err => console.log(err))
+})
+
 module.exports = router
