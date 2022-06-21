92c49c921ac0438ab0fa0b8923b0907e0e371315
june850713@gmail.com <june850713@gmail.com>
2022-06-22 02:07:32 +0800

    feat: add register function

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const router = express.Router()
+const User = require('../../models/user')
 
 router.get('/login', (req, res) => {
   res.render('login')
@@ -7,6 +8,21 @@ router.get('/login', (req, res) => {
 
 router.get('/register', (req, res) => {
   res.render('register')
+})
+
+router.post('/register', (req, res) => {
+  const { name, password, confirmPassword } = req.body
+  User.findOne({ name })
+    .then(user => {
+      if (user) {
+        console.log('user exist')
+        return res.render('register')
+      }
+      User.create({ name, password })
+        .then(() => res.redirect('/'))
+        .catch(err => console.log(err))
+    })
+    .catch(err => console.log(err))
 })
 
 module.exports = router
