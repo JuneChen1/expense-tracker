a34a382bfeb7ade53da950bda36f8f6153947d1e
june850713@gmail.com <june850713@gmail.com>
2022-06-21 13:37:25 +0800

    feat: add express-handlebars

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 const express = require('express')
+const exphbs = require('express-handlebars')
 const routes = require('./routes')
 
 const app = express()
 
 const port = 3000
+
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
 
 app.use(routes)
 
