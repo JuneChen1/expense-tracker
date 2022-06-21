68ef351297bba399217c768ac7b4d03de4546d56
june850713@gmail.com <june850713@gmail.com>
2022-06-21 22:31:37 +0800

    feat: render index page by record model

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -1,8 +1,26 @@
 const express = require('express')
 const router = express.Router()
+const Record = require('../../models/record')
+const Category = require('../../models/category')
 
-router.get('/', (req, res) => {
-  res.render('index')
+router.get('/', async (req, res) => {
+  const categories = await Category.find()
+  console.log(categories)
+  Record.find()
+    .sort({ 'date': 'asc'})
+    .lean()
+    .then(records => {
+      records.forEach(record => {
+        let year = record.date.getYear() + 1900
+        let month = record.date.getMonth() + 1
+        let date = record.date.getDate().toString()
+        record.date = year + '/' + month + '/' + date
+        const icon = categories.find(category => record.categoryId === category.id).icon
+        record.icon = icon
+      })
+      res.render('index', { records })
+    })
+    .catch(err => console.log(err))
 })
 
 module.exports = router
--- routes/modules/rocord.js
+++ routes/modules/rocord.js
@@ -1,23 +1,19 @@
 const express = require('express')
 const router = express.Router()
 const CATEGORY = require('../../category.json').data
+const Record = require('../../models/record')
+const Category = require('../../models/category')
 
 // add new record
-router.get('/new', (req, res) => {
-  const categorys = []
-  CATEGORY.forEach(category => {
-    categorys.push({
-      id: category.id,
-      name: category.name
-    })
-  })
+router.get('/new', async (req, res) => {
+  const categorys = await Category.find().lean().sort({ 'id': 'asc' })
   res.render('new', { categorys })
 })
 
 router.post('/new', (req, res) => {
-  const record = req.body
-  console.log(record)
-  res.redirect('/')
+  Record.create(req.body)
+    .then(() => res.redirect('/'))
+    .catch(err => console.log(err))
 })
 
 module.exports = router
