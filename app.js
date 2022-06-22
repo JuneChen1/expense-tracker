0c230fcccbd3468a7d1891f531ffedc59c105f98
june850713@gmail.com <june850713@gmail.com>
2022-06-22 13:04:00 +0800

    feat: add user login feature via passport

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const exphbs = require('express-handlebars')
 const methodOverride = require('method-override')
 const session = require('express-session')
+const usePassport = require('./config/passport')
 const routes = require('./routes')
 
 const app = express()
@@ -17,6 +18,7 @@ app.use(session({
   resave: false,
   saveUninitialized: true
 }))
+usePassport(app)
 app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
 app.use(routes)
--- routes/modules/users.js
+++ routes/modules/users.js
@@ -1,11 +1,13 @@
 const express = require('express')
 const router = express.Router()
 const bcrypt = require('bcryptjs')
+const passport = require('passport')
 const User = require('../../models/user')
 
-router.get('/login', (req, res) => {
-  res.render('login')
-})
+router.get('/login', passport.authenticate('local', {
+  successRedirect: '/',
+  failureRedirect: '/users/login'
+}))
 
 router.get('/register', (req, res) => {
   res.render('register')
