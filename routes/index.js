26efc637202b1d07ccd3586a11558586622edda8
june850713@gmail.com <june850713@gmail.com>
2022-06-22 13:13:55 +0800

    feat: add user authentication

--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,10 @@ const router = express.Router()
 const home = require('./modules/home')
 const recrods = require('./modules/rocords')
 const users = require('./modules/users')
+const { authenticator } = require('../middleware/auth')
 
-router.use('/records', recrods)
+router.use('/records', authenticator, recrods)
 router.use('/users', users)
-router.use('/', home)
+router.use('/', authenticator, home)
 
 module.exports = router
